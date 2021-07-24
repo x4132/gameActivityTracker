ef81cbaea01969a7770f29b6fc772e0306a840fe
bluninja1234 <alexyiheng@gmail.com>
2021-07-24 13:16:33 -0700

    add navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+
 import Navbar from "./jsx/Navbar";
+import Dashboard from "./jsx/Dashboard";
 
 import "./css/main.css";
 
 function App() {
   return (
-    <div className="w-100 h-100 bg-dark" >
-      <Navbar />
-    </div>
+    <Router>
+      <div className="w-100 h-100 bg-dark text-light" >
+        <Navbar />
+      </div>
+      <Switch>
+        <Route path="/" component={Dashboard} />
+      </Switch>
+    </Router>
   );
 }
 
--- client/src/jsx/Navbar.tsx
+++ client/src/jsx/Navbar.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
-    return <div className="border-bottom" >
-        
-    </div>
+    return <nav className="navbar navbar-expand navbar-dark bg-dark border-bottom p-2" >
+        <div className="container-fluid" >
+            <Link to="/" className="navbar-brand" >Time Tracker</Link>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
+                <li className="nav-item" >
+                    <Link to="/history" className="nav-link" >History</Link>
+                </li>
+                <li className="nav-item" >
+                    <Link to="/addgame" className="nav-link" >Add a game</Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
 }
